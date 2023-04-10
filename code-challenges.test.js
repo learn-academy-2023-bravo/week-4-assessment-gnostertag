4e988f10eb0a2280fd8e06e06ece6d63ca30207d
gnostertag <ostertag2000@gmail.com>
2023-04-10 07:00:13 -0700

    week4

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,37 +15,124 @@
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = [
-  "chartreuse",
-  "indigo",
-  "periwinkle",
-  "ochre",
-  "aquamarine",
-  "saffron"
-]
+
+describe ('arrayShuffle', () => {
+  const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+
+  const colors2 = [
+    "chartreuse",
+    "indigo",
+    "periwinkle",
+    "ochre",
+    "aquamarine",
+    "saffron"
+  ]
+
+  it('removes the first item from an array and shuffle the remaining content', () => {
+    expect(arrayShuffle (colors1)).toEqual(expect.arrayContaining(["blue", "green", "yellow", "pink"]))
+    expect(arrayShuffle(colors2)).toEqual(expect.arrayContaining(["aquamarine", "periwinkle", "indigo", "ochre"]))
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+//   arrayShuffle
+//     ✕ removes the first item from an array and shuffle the remaining content (4 ms)
+
+//   ● arrayShuffle › removes the first item from an array and shuffle the remaining content
+
+//     ReferenceError: arrayShuffle is not defined
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
 // b) Create the function that makes the test pass.
+
+//pseudo
+//input: array
+//output: array with the 0 index removed and the remaining content shuffled.
+//First we will create a function called arrayShuffle, then I want to use the .shift method to remove the first value from the array. Then I want to create a random comparison function using the .sort and Math.random. When math.random is used in the sort method, I'm expecting it to shuffle the elements of the input array randomly. after researching some more about math.random, I learned that it generates a random number between 0 and 1. Therefore if we subtract 1 from this random number, we will shift the possible range of values down by 1. Additionally if the range of possible values is -1 to 1, this should mean we get a random shuffle. This should return the new subset array, shuffled.
+
+const arrayShuffle = (arr) => {
+      arr.shift()
+      return arr.sort(() => Math.random()-1 && 1)
+}
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
-const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+describe("voteTally", () => {
+  
+  const votes1 = { upVotes: 13, downVotes: 2 }
+  const votes2 = { upVotes: 2, downVotes: 33 }
+
+  it("return the total number of votes tallied", () =>{
+    expect((voteTally)(votes1)).toEqual(11)
+    expect((voteTally)(votes2)).toEqual(-31)
+
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+//   voteTally
+//     ✕ return the total number of votes tallied (1 ms)
+
+//   ● voteTally › return the total number of votes tallied
+
+//     ReferenceError: voteTally is not defined
+
 // b) Create the function that makes the test pass.
+//pseudo
+//Input: object
+//Output: end tally of votes
+// I'll first create a function called voteTally, I know that this function will be inputting an object so thats what the parameter will be. Then I want to declare that the tally is the number of upvotes minus the number of downvotes. Fnally tally will be retuned.
+
+const voteTally = (obj) => {
+ let tally = obj.upVotes - obj.downVotes
+ return tally
+}
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const dataArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
+describe("noDupes", () => {
+
+  const dataArray1 = ["array", "object", "number", "string", "Boolean"]
+  const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
+
+it("returns one array with no duplicate values", () => {
+expect ((noDupes)(dataArray1, dataArray2)).toEqual (["array", "object", "number", "string", "Boolean", "null", "undefined"])
+})
+})
+
+// FAIL  ./code-challenges.test.js
+//   noDupes
+//     ✕ returns one array with no duplicate values (1 ms)
+
+//   ● noDupes › returns one array with no duplicate values
+
+//     ReferenceError: noDupes is not defined
+
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
 // b) Create the function that makes the test pass.
+//Pseudo
+//input: two arrays
+//output: one array with no duplicate values
+//I'll first create a function called noDupes. Then i need a way to concat the two arrays into one array. if i declare the new array equal to 1 .concat 2, that should do it. Then take that new array and use the filter method to remove the duplicate values. Filter will apply a callback function to the array we just made and will return true if the value should be in the new array and false if it shouldnt. So we can use filter with indexOf to check the index position of the first element and compare it to the index position of its first occurrance. If they are the same then the value is unique. Thus an array with no dupes should be returned.
+
+const noDupes = (data1, data2) => {
+  let newData = data1.concat(data2)
+  return newData.filter((value, index) => newData.indexOf(value) === index)
+}
+
+const noDupes = (data1, data2) => {
+  let combinedArr = [...data1, ...data2]
+  //Spread operator combines the 2 arrays into 1.
+  let filterArr = [...new Set (combinedArr)]
+  //researched the Set object and found it can store unique values
+  return filterArr
+}
--- interview-questions.md
+++ interview-questions.md
@@ -8,42 +8,52 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: Otherwise known as OOP, object-oriented programming is a way to make code more organized and easier to work with. When using OOP we group similar types of data together into objects and give them names. then we can call on these objects to do certain tasks.
 
-Researched answer:
+Researched answer: Often called OOP, object-oriented programming was created to deal with large scale projects that have many components. Over the years apps have grown in size and developers needed a way to make changes to the data without affecting the entire code. One small change in a component file could trigger a domino effect of errors, and be a pain to debug. OOP lets us section off different areas of code so that projects become a collection of small parts. Think of a puzzle, and building each corner at a time, then putting the whole thing together for a finished product.
 
-1. What is the difference between a Float and an Integer in Ruby?
+2. What is the difference between a Float and an Integer in Ruby?
 
-Your answer:
+Your answer: A float is very similar to an integer. A float is a decimal number and an integer would be a whole number. A float will give us different outputs than a whole number. For example, a integer will will always output a whole number and a float will give us a decimal.
 
-Researched answer:
+Researched answer: A float is a number that has a decimal point and can have a fractional part. In ruby to declare a float, just write any number with a decimal point. An integer is a whole number that has no fractional parts. for example, 3 is an integer while 3.0 is a float. Floats are good for storing very small or very large values. In ruby if you want to return a non whole number you need to use a float. To show the differnce in outputs consider this... 
+3 / 2  will  output 1, while 3.0 / 2 will output: 1.5. the float gave us the more precise answer.
 
-1. Ruby has an implicit return. What does that mean?
+3. Ruby has an implicit return. What does that mean? 
 
-Your answer:
+Your answer: An implicit return is when there is no return statement, the value of the value of the last line of the method is returned. Ruby is a language that uses implicit returns, therefore using the keyword return, is not always necessary.
 
-Researched answer:
+Researched answer: Ruby is a language that uses implicit returns, this basically means that if there is no specified return, the value of the last method used will be returned. Implicit returns can make code shorter, but I can see how it could make it harder to read.
 
-1. What is a block in Ruby?
+4. What is a block in Ruby?
 
-Your answer:
+Your answer: A block in ruby is a feature of ruby that passes a function through a method to determine the output. do and end are the keywords that define a block.
 
-Researched answer:
+Researched answer: A ruby block is a snippet of code that can be passed as an argument to a method. It is an anonymous function and it is usually defined with the key words do and end. A block is a piece of code that can be passed to a method and used in the context of that method.
 
-1. How do you extract a value in a Ruby hash?
 
-Your answer:
+5. How do you extract a value in a Ruby hash?
 
-Researched answer:
+Your answer: To extract a value from a hash in ruby you would use square brackets [] and the name of the key. It would look something like this ```    
+student = Hash.new
+student[:name] = 'Greg'
+student[:home] = 'Minnesota'
+p student[:name]
+## 'Greg'
+
+```
+We extracted tha value 'Greg' by using the class of student , and the name of the key in square brackets.
+
+Researched answer: In Ruby you extract the value from a hash by using the key that is associated with the value. The easiest way to do this is by using square brackets and the name of the key.
 
 ## Looking Ahead: Terms for Next Week
 
-1. RSpec:
+1. RSpec: RSpec is a testing frmework for Ruby. It is a behavior driven development and it allows us to describe the behavior of the code and make sure that it behaves as expected. Like all things in Ruby, RSpec is designed to be easy to read and write.
 
-2. Test-driven development:
+2. Test-driven development: This is a programming process that focuses on writing an automated test before writing the actual code. We saw this when working with javascript, and would test our code using jest.
 
-3. PostgreSQL:
+3. PostgreSQL: PostgreSQL is an open sourced database management system. This means that it looks at databases through an object oriented programming lense
 
-4. CRUD:
+4. CRUD: CRUD is an acronym that stands for create, read, update, delete. These are the four general actions that are performed on data when programming. 
 
-5. HTTP:
+5. HTTP: This is a tool used for transferring data over the internet. It is the foundation of data communication on the World Wide Web and is used to enable communication between clients and servers. Whenever you type in a website name, an http request is sent to the server hosting that website.
